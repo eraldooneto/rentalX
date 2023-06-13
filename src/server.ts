2d5329e1646195cb8c458c99c56586b7aab2375f
Eraldo <jesn@ic.ufal.br>
2023-06-13 19:02:18 -0300

    feat: class Category added and users can creat a new category

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,12 @@
 import express from 'express';
+import { categoriesRoutes } from './routes/categories.routes';
 
 const app = express();
 const port = 3333;
 
 app.use(express.json());
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello World' });
-});
-
-app.post('/courses', (request, response) => {
-    const { name } = request.body;
-    return response.json({ name: name });
-});
+app.use('/categories', categoriesRoutes);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
