5648114aa6b39c4c30eeb9f773f1edaf70a887e3
Eraldo <jesn@ic.ufal.br>
2023-06-13 14:45:53 -0300

    build: configs for debugger added

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,16 @@ import express from 'express';
 const app = express();
 const port = 3333;
 
+app.use(express.json());
+
 app.get('/', (request, response) => {
     return response.json({ message: 'Hello World' });
 });
 
+app.post('/courses', (request, response) => {
+    const { name } = request.body;
+    return response.json({ name: name });
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
