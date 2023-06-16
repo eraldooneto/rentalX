a844284e8aabdac6d2b36e52e10fe3ba02953481
Eraldo <jesn@ic.ufal.br>
2023-06-16 20:34:31 -0300

    feat: Specification module was developed

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,10 +1,21 @@
+import { ISpecificationsRepository } from "../repositories/ISpecificationsRepository";
 
-
+interface IRequest {
+    name: string;
+    description: string;
+};
 
 class CreateSpecificationService {
-    execute() {
-        console.log("TODO");
+    constructor(private specificationsRepository: ISpecificationsRepository) {};
+
+    execute({ name, description }: IRequest): void {
+        const nameAlreadyExists = this.specificationsRepository.findByName(name);
         
+        if (nameAlreadyExists) {
+            throw new Error("Name already exists!");
+        }
+
+        this.specificationsRepository.create({ name, description });
     }
 
 };
