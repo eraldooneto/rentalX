79bce83474e6d38a644edb9802e0502e44e5e8d5
Eraldo <jesn@ic.ufal.br>
2023-06-18 19:27:13 -0300

    feat: the route categories/import was developed to upload files to the application.

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -3,14 +3,27 @@ import { Router } from 'express';
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
 import { listCategoryController } from '../modules/cars/useCases/listCategory';
 
+import multer from 'multer';
+
 const categoriesRoutes = Router();
+const upload = multer({
+     dest: './tmp',
+});
 
 categoriesRoutes.post("/", (request, response) => {
     return createCategoryController.handle(request, response);
 });
 
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+    const { file } = request;
+    console.log(file);
+    
+    return response.send();
+});
+
 categoriesRoutes.get("/", (request, response) => {
     return listCategoryController.handle(request, response);
 });
+
 
 export { categoriesRoutes };
