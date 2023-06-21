b5f02685a883bccd31dedf40301cf8387351936d
Eraldo <jesn@ic.ufal.br>
2023-06-21 20:47:29 -0300

    docs: documentation files using swagger

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
 import express from 'express';
 import { router } from './routes';
 
+import swaggerUi from 'swagger-ui-express';
+
+import swaggerDocument from './swagger.json';
+
+
 const app = express();
 const port = 3333;
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(router);
 
