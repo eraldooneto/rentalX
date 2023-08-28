41b9537a10bda8eec9d06ee643fca7d5e9ab9ef5
Eraldo <jesn@ic.ufal.br>
2023-08-28 00:54:41 -0300

    feat: authentication has been implemented on specification module

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -2,13 +2,14 @@ import { Router } from "express";
 
 import { ListSpecificationController } from "../modules/cars/useCases/listSpecification/ListSpecificationController";
 import { CreateSpecificationController } from "../modules/cars/useCases/createSpecification/CreateSpecificationController";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const specificationsRoutes = Router(); 
 
 const createSpecificationController = new CreateSpecificationController();
 const listSpecificationController = new ListSpecificationController();
 
-
+specificationsRoutes.use(ensureAuthenticated);
 specificationsRoutes.post("/", createSpecificationController.handle);
 
 specificationsRoutes.get("/", listSpecificationController.handle);
