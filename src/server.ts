852ad72bf79bdca6526637c08d2ef12927ad556a
Eraldo <jesn@ic.ufal.br>
2023-09-29 16:13:43 -0300

    fix: bcrypt error fixed

--- src/server.ts
+++ src/server.ts
@@ -5,11 +5,11 @@ import "express-async-errors";
 import { router } from './routes';
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from './swagger.json';
-import { AppError } from './errors/AppError';
 
 import './database';
 
 import './shared/container';
+import { AppError } from '@errors/AppError';
 
 
 const app = express();
