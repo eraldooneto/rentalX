b8df1b7f5ae41b4d5f1c6a6d8931d642a620f67d
Eraldo <jesn@ic.ufal.br>
2023-10-04 22:50:09 -0300

    dev: application details

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,4 +1,4 @@
-import { container } from "tsyringe"; 
+import { container } from "tsyringe";
 
 import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
@@ -6,6 +6,8 @@ import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRep
 import { SpecificationsRepository } from "@modules/cars/infra/typeorm/repositories/SpecificationsRepository";
 import { ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationsRepository";
 import { CategoriesRepository } from "@modules/cars/infra/typeorm/repositories/CategoriesRepository";
+import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
+import { CarsRepository } from "@modules/cars/infra/typeorm/repositories/CarsRepository";
 
 container.registerSingleton<ICategoriesRepository>(
     "CategoriesRepository",
@@ -20,5 +22,10 @@ container.registerSingleton<ISpecificationsRepository>(
 container.registerSingleton<IUsersRepository>(
     "UsersRepository",
     UsersRepository
+);
+
+container.registerSingleton<ICarsRepository>(
+    "CarsRepository",
+    CarsRepository
 );
 
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -4,6 +4,7 @@ import { categoriesRoutes } from './categories.routes';
 import { usersRoutes } from './users.routes';
 import { specificationsRoutes } from './specifications.routes';
 import { authenticateRoutes } from './authenticate.routes';
+import { carsRoutes } from './cars.routes';
 
 
 const router = Router();
@@ -12,5 +13,6 @@ router.use('/categories', categoriesRoutes);
 router.use('/users', usersRoutes)
 router.use('/specifications', specificationsRoutes);
 router.use(authenticateRoutes);
+router.use('/cars', carsRoutes);
 
 export { router };
